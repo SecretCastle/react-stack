9f54d3c65d2949f8beb0912a4ca4deea9d656344
SecretCastle <chenfangzhong@hotmail.com>
2018-10-29 09:42:15 +0800

    :rocket: fix problems

--- src/components/pages/TimePicker/index.js
+++ src/components/pages/TimePicker/index.js
@@ -4,13 +4,19 @@ import MultiTimePicker from '@/public/TimePicker';
 
 const Fragment = React.Fragment;
 class index extends Component {
+    select = data => {
+        console.log(data);
+    }
     render() {
         return (
             <Fragment>
                 <span>Base On Ant-Design</span>
                 <Tabs>
                     <Tabs.TabPane tab="MultiTimePicker" key="1">
-                        <MultiTimePicker />
+                        <MultiTimePicker 
+                            valid={true}
+                            callback={this.select}
+                        />
                     </Tabs.TabPane>
                 </Tabs>
             </Fragment>
--- src/public/TimePicker/index.js
+++ src/public/TimePicker/index.js
@@ -1,24 +1,131 @@
 import React, { Component } from 'react';
-import { TimePicker, Button, Icon } from 'antd';
+import { TimePicker, Button, Icon, Modal} from 'antd';
 import './index.less';
 
 class MultiTimePicker extends Component {
     constructor() {
         super();
         this.state = {
-            open: false
+            open: false,
+            btnDisable: false,
+            startTime: {
+                moment: null,
+                value: null
+            },
+            endTime: {
+                moment: null,
+                value: null
+            },
+            modal: {
+                startError: null,
+                endError: null
+            }
         }
     }
     confirm = () => {
-        this.setState({
-            open: false
-        });
+        const { callback } = this.props;
+        if (this.state.startTime.moment === null || this.state.endTime.moment === null) {
+            Modal.info({
+                title: '请选择开始或结束时间',
+                okText: '确定'
+            });
+            return;
+        } else {
+            this.setState({
+                open: false
+            });
+            callback({
+                startTime: this.state.startTime, 
+                endTime: this.state.endTime
+            });
+        }
+        
     }
-    openChange() {
-        console.log(this.state.open);
+    openChange = () => {
         this.setState({
             open: true
         });
+    }
+    onChange(moment, type) {
+        const { valid } = this.props;
+        console.log(type);
+        if (valid) {
+            if (type === '1') {
+                this.setState({
+                    startTime: {
+                        moment: moment,
+                        value: moment.format('HH:mm:ss')
+                    }
+                });
+                if (this.state.endTime.moment !== null) {
+                    const endTimeMoment = this.state.endTime.moment;
+                    if (moment.isAfter(endTimeMoment)) {
+                        if (this.state.modal.startError) {
+                            this.state.modal.startError.destroy();
+                        }
+                        const startError = Modal.error({
+                            title: '开始时间不能小于结束时间',
+                            okText: '确定'
+                        });
+                        this.setState({
+                            btnDisable: true,
+                            modal: {
+                                startError
+                            }
+                        });
+                    } else {
+                        this.setState({
+                            btnDisable: false
+                        })
+                    }
+                }
+            } else {
+                this.setState({
+                    endTime: {
+                        moment: moment,
+                        value: moment.format('HH:mm:ss')
+                    }
+                });
+                if (this.state.startTime.moment !== null) {
+                    const startMoment = this.state.startTime.moment;
+                    if (moment.isBefore(startMoment)) {
+                        if (this.state.modal.endError) {
+                            this.state.modal.endError.destroy();
+                        }
+                        const endError = Modal.error({
+                            title: '结束时间不能大于开始时间',
+                            okText: '确定'
+                        });
+                        this.setState({
+                            btnDisable: true,
+                            modal: {
+                                endError
+                            }
+                        })
+                    } else {
+                        this.setState({
+                            btnDisable: false
+                        })
+                    }
+                }
+            }
+        } else {
+            if (type === '1') {
+                this.setState({
+                    startTime: {
+                        moment: moment,
+                        value: moment.format('HH:mm:ss')
+                    }
+                })
+            } else {
+                this.setState({
+                    endTime: {
+                        moment: moment,
+                        value: moment.format('HH:mm:ss')
+                    }
+                })
+            }
+        }
     }
     render() {
         return (
@@ -27,7 +134,9 @@ class MultiTimePicker extends Component {
                     open={this.state.open}
                     format="HH:mm:ss"
                     style={{ width: '165px' }}
-                    onOpenChange={() => this.openChange()}
+                    onOpenChange={this.openChange}
+                    onChange={(moment) => this.onChange(moment, '1')}
+                    placeholder="开始时间"
                     addon={() => (
                         <span style={{ height: '24px', display: 'block' }}></span>
                     )}
@@ -37,10 +146,12 @@ class MultiTimePicker extends Component {
                     open={this.state.open}
                     format="HH:mm:ss"
                     style={{ width: '165px' }}
-                    onOpenChange={() => this.openChange()}
+                    onChange={(moment) => this.onChange(moment, '2')}
+                    onOpenChange={this.openChange}
+                    placeholder="结束时间"
                     addon={() => (
                         <div style={{ textAlign: 'right' }}>
-                            <Button onClick={this.confirm} size="small">确定</Button>
+                            <Button disabled={this.state.btnDisable} onClick={this.confirm} size="small">确定</Button>
                         </div>
                     )}
                 />
